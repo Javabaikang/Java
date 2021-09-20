de9c46d816d82a93a241381401d7cbf767286709
chenfangkang <18755196482@163.com>
2021-09-20 14:59:20 +0800

    demo案例

---
+++ SpringSecurity/src/main/java/com/chenfangkang/service/MyService.java
@@ -0,0 +1,11 @@
+package com.chenfangkang.service;
+
+import org.springframework.security.core.Authentication;
+
+import javax.servlet.http.HttpServletRequest;
+
+public interface MyService {
+
+    boolean hasPermission(HttpServletRequest request, Authentication authentication);
+}
+
